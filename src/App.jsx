41fcb976f3d0a8be46d5e478585fe24a78dfd02f
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-10 10:31:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h2 className="bg-green-500"> React router</h2>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+
+// * Two types of creating an Routes --->
+
+// ?  1)
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+
+//       {
+//         path: "contact-us",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ?  2)
+
+// * Usually below syntax is easier and recomended;
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact-us" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
